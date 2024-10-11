effb348d3edd1c23fbcd98e0281dcefa33bc403e
tomeytran <tmtri310251@gmail.com>
2024-10-11 11:33:51 +0700

    Reverse array

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -3,34 +3,41 @@ import java.util.Scanner;
 public class ReverseArray {
 
     public static void main(String[] args) {
-        int size;
-        int[] array;
-        Scanner scanner = new Scanner(System.in);
+        int size = 0;
+        int[] arr;
+        Scanner input = new Scanner(System.in);
+        /*
+        System.out.print("Enter a size of array = ");
+        size = input.nextInt();
+        while (size > 20) {
+            System.out.print("Enter a size of array = ");
+            size = input.nextInt();
+        }
+        */
+        // Enter size < 20
         do {
-            System.out.print("Enter a size:");
-            size = scanner.nextInt();
+            System.out.print("Enter a size: ");
+            size = input.nextInt();
             if (size > 20)
                 System.out.println("Size does not exceed 20");
         } while (size > 20);
-        array = new int[size];
-        int i = 0;
-        while (i < array.length) {
-            System.out.print("Enter element " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
-            i++;
+
+        // Enter elements' value
+        arr = new int[size];
+        for (int i = 0; i < arr.length; i++){
+            System.out.print("Enter " + (i+1) + " element: ");
+            arr[i] = input.nextInt();
         }
-        System.out.printf("%-20s%s", "Elements in array: ", "");
-        for (int j = 0; j < array.length; j++) {
-            System.out.print(array[j] + "\t");
+
+        // Print out 2 arrays
+        System.out.print("Original array: ");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
-        for (int j = 0; j < array.length / 2; j++) {
-            int temp = array[j];
-            array[j] = array[size - 1 - j];
-            array[size - 1 - j] = temp;
-        }
-        System.out.printf("\n%-20s%s", "Reverse array: ", "");
-        for (int j = 0; j < array.length; j++) {
-            System.out.print(array[j] + "\t");
+        System.out.println(" ");
+        System.out.print("Reversed array: ");
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
         }
     }
 }
